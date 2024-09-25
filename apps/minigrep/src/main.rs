6bc8b5ddd7f4010de4caae7ab200941a9b2ebfca
pratik-codes <pratik2018id@gmail.com>
2024-09-26 01:31:04 +0530

    feat: made minigrep modular and added error handling

--- apps/minigrep/src/main.rs
+++ apps/minigrep/src/main.rs
@@ -1,12 +1,24 @@
+use std::process;
+
 mod utils;
 
 fn main() {
     println!("Mini grep by Pratik...");
 
     let args: Vec<String> = utils::get_command_line_args();
-    let config = utils::parse_config(&args);
-    let file_content: String = utils::read_file_content(&config.file_path);
-
+    let config = utils::Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
+    let file_content = match utils::read_file_content(&config.file_path) {
+        Ok(content) => content, // If successful, assign the file content
+        Err(err) => match err {
+            err => {
+                println!("Error reading the file: {}", err);
+                std::process::exit(1);
+            }
+        },
+    };
     println!("Searching for {}", config.query);
     println!("In file {}", config.file_path);
     println!("File content: {:?}", file_content);
--- apps/minigrep/src/utils.rs
+++ apps/minigrep/src/utils.rs
@@ -1,9 +1,25 @@
 use std::env;
 use std::fs;
+use std::io::Error;
 
 pub struct Config {
     pub query: String,
     pub file_path: String,
+}
+
+// parse the args to get config
+// using clone we loose a little on speed and performance but we gain on code redablitiy by not
+// introducing lifetimes
+impl Config {
+    pub fn build(args: &[String]) -> Result<Config, &str> {
+        if args.len() < 3 {
+            return Err("not enought arguments");
+        }
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+
+        Ok(Config { query, file_path })
+    }
 }
 
 pub fn get_command_line_args() -> Vec<String> {
@@ -11,15 +27,10 @@ pub fn get_command_line_args() -> Vec<String> {
     env::args().collect()
 }
 
-// parse the args to get config
-pub fn parse_config(args: &[String]) -> Config {
-    let query = args[1].clone();
-    let file_path = args[2].clone();
-
-    Config { query, file_path }
-}
-
-pub fn read_file_content(file_path: &String) -> String {
-    let content = fs::read_to_string(file_path).expect("should have been able to read the file");
-    content
+pub fn read_file_content(file_path: &String) -> Result<String, Error> {
+    let content = fs::read_to_string(file_path);
+    match content {
+        Ok(data) => Ok(data),
+        Err(err) => Err(err), // Propagate the error
+    }
 }
